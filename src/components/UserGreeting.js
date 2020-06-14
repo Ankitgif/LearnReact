65df28041cd576a087736103b1da3af791229a2b
Ankitgif <ankitsinha2303@gmail.com>
2020-06-15 03:27:51 +0530

    [Ankit] Conditional rendering using Short circuit operator

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -12,12 +12,14 @@ class UserGreeting extends Component {
 
     render() {
 
-        return(
-        this.state.isLoggedIn ? (
-            <div>Welcome Ankit</div> 
-         ) : (
-            <div>Welcome Guest</div>
-        ))
+        return this.state.isLoggedIn && <div>Welcome Ankit</div>
+
+        // return(
+        // this.state.isLoggedIn ? (
+        //     <div>Welcome Ankit</div> 
+        //  ) : (
+        //     <div>Welcome Guest</div>
+        // ))
         // let message
         // if(this.state.isLoggedIn){
         //     message = <div>Welcome Ankit</div>
