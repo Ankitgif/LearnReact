3acaad7262b476a0708e9a443f02c1c95227e4f1
Ankitgif <ankitsinha2303@gmail.com>
2020-06-15 03:48:08 +0530

    [Ankit] List Rendering approach 2

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,11 +1,29 @@
 import React from 'react'
 
 function NameList() {
-    const names = ['Bruce', 'Clark', 'Diana']
-    const nameList = names.map(name => <h2>{name}</h2>) 
+    const persons = [
+        {
+            id: 1,
+            name: 'Bruce',
+            age: 30,
+            skill: 'React'
+        },
+        {
+            id: 2,
+            name: 'Clark',
+            age: 25,
+            skill: 'Angular'
+        },
+        {
+            id: 3,
+            name: 'Diana',
+            skill: 'Vue'
+        }
+    ]
+    const personList = persons.map(person => <h2>I am {person.name}. I am {person.age}. I know {person.skill}</h2>)
     return (
         <div>
-           {nameList}
+            {personList}
         </div>
     )
 }
