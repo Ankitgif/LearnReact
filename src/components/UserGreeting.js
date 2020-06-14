f4b68fd43ed763d58beba8e7aba66937a0dadf2a
Ankitgif <ankitsinha2303@gmail.com>
2020-06-15 03:24:08 +0530

    [Ankit] Conditional rendering using ternary operator approach

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -4,27 +4,33 @@ class UserGreeting extends Component {
 
     constructor(props) {
         super(props)
-    
+
         this.state = {
-             isLoggedIn: false
+            isLoggedIn: true
         }
     }
-    
+
     render() {
 
-        let message
-        if(this.state.isLoggedIn){
-            message = <div>Welcome Ankit</div>
-        }else {
-            message = <div>welcome Guest</div>
-        }
+        return(
+        this.state.isLoggedIn ? (
+            <div>Welcome Ankit</div> 
+         ) : (
+            <div>Welcome Guest</div>
+        ))
+        // let message
+        // if(this.state.isLoggedIn){
+        //     message = <div>Welcome Ankit</div>
+        // }else {
+        //     message = <div>welcome Guest</div>
+        // }
         // if(this.state.isLoggedIn) {
         //     return <div>Welcome Ankit</div>
         // }else {
         //     return <div>Welcome Guest</div>
         // }
-           
-        return <div>{message}</div>
+
+        // return <div>{message}</div>
         // return (
         //     <div>
         //        <div>Welcome Ankit</div>
