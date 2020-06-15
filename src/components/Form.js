272a460e080dce46a6b6095b47584e38eae2d4e2
Ankitgif <ankitsinha2303@gmail.com>
2020-06-15 14:39:55 +0530

    [Ankit] Basics of Form Handeling destructuring

--- src/components/Form.js
+++ src/components/Form.js
@@ -36,23 +36,24 @@ class Form extends Component {
     }
     
     render() {
+        const { username, comments, topic } = this.state
         return (
             <form onSubmit={this.handleSubmit}>
                 <div>
                     <label>Username</label>
-                    <input type='text' value={this.state.username} 
+                    <input type='text' value={username} 
                     onChange={this.handleUsernameChange}
                     />               
                 </div>
                 <div>
                     <label>Comments</label>
-                    <textarea value={this.state.comments}
+                    <textarea value={comments}
                     onChange={this.handleCommentsChange}
                     />
                 </div>
                 <div>
                     <label>Topic</label>
-                    <select value={this.state.topic} onChange={this.handleTopicChange}>
+                    <select value={topic} onChange={this.handleTopicChange}>
                         <option value="react">React</option>
                         <option value="angular">Angular</option>
                         <option value="vue">Vue</option>
